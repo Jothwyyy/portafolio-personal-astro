4a70a61fefd5a9f3bdd1b14469b6c0cb5e43be92
Jothwyyy <1724110287@utectulancingo.edu.mx>
2026-05-31 11:57:24 -0600

    refactor: organizar estructura de datos de la navbar

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,7 +1,31 @@
 export const navigation = [
-	{ href: '/', label: 'Inicio' },
-	{ href: '/left-sidebar', label: 'Left Sidebar' },
-	{ href: '/right-sidebar', label: 'Right Sidebar' },
-	{ href: '/no-sidebar', label: 'No Sidebar' },
-	{ href: '/contact', label: 'Contacto' },
+  { 
+    href: '/', 
+    label: 'Welcome', 
+    class: 'current' 
+  },
+  {
+    label: 'Layouts',
+    layouts: [
+      { href: '/left-sidebar', label: 'Left Sidebar' },
+      { href: '/right-sidebar', label: 'Right Sidebar' },
+      { href: '/no-sidebar', label: 'No Sidebar' },
+      { href: '/contact', label: 'Contact' },
+      {
+        label: 'Submenu',
+        submenu: [
+          { href: '', label: 'Dolore Sed' },
+          { href: '', label: 'Consequat' },
+          { href: '', label: 'Lorem Magna' },
+          { href: '', label: 'Sed Magna' },
+          { href: '', label: 'Ipsum Nisl' },
+        ],
+      },
+    ],
+  },
+  {
+	label: 'Sign Up',
+	href: '#',
+	class: 'button primary',
+  }
 ];
