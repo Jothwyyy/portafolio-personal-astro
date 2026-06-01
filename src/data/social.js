081c5738c273f0a7224577b2c0cf2c39f85f17c6
Jothwyyy <1724110287@utectulancingo.edu.mx>
2026-05-31 20:27:51 -0600

    style: alinear los iconos sociales del footer como en la plantilla

--- src/data/social.js
+++ src/data/social.js
@@ -1,5 +1,7 @@
 export const socialLinks = [
-	{ href: '#', label: 'Twitter' },
-	{ href: '#', label: 'GitHub' },
-	{ href: '#', label: 'LinkedIn' },
+	{ href: '#', label: 'Twitter', iconClass: 'icon brands circle fa-twitter'},
+	{ href: '#', label: 'Facebook', iconClass: 'icon brands circle fa-facebook-f'},
+	{ href: '#', label: 'Google+', iconClass: 'icon brands circle fa-google-plus-g'},
+	{ href: '#', label: 'GitHub', iconClass: 'icon brands circle fa-github'},
+	{ href: '#', label: 'Dribbble', iconClass: 'icon brands circle fa-dribbble'},
 ];
